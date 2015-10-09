9983a6b76f89545e91bbfea80a80a4c7c5c773af
juanprietob <juanprietob@gmail.com>
2015-10-09 15:06:27 -0400

    BUG: Include error use itksys for hash map

--- src/lib/itkLabelGeometry/itkLabelGeometryImageFilter.h
+++ src/lib/itkLabelGeometry/itkLabelGeometryImageFilter.h
@@ -29,7 +29,7 @@ can be obtained from http://nihroadmap.nih.gov/bioinformatics.
 #include "itkNumericTraits.h"
 #include "itkArray.h"
 #include "itkSimpleDataObjectDecorator.h"
-#include "itk_hash_map.h"
+#include "itksys/hash_map.hxx"
 #include "itkFastMutexLock.h"
 #include <vector>
 #include <vnl/algo/vnl_symmetric_eigensystem.h>
@@ -229,9 +229,9 @@ public:
   
   /** Type of the map used to store data per label */
   // Map from the label to the class storing all of the geometry information.
-  typedef itk::hash_map<LabelPixelType, LabelGeometry> MapType;
-  typedef typename itk::hash_map<LabelPixelType, LabelGeometry>::iterator MapIterator;
-  typedef typename itk::hash_map<LabelPixelType, LabelGeometry>::const_iterator MapConstIterator;
+  typedef itksys::hash_map<LabelPixelType, LabelGeometry> MapType;
+  typedef typename itksys::hash_map<LabelPixelType, LabelGeometry>::iterator MapIterator;
+  typedef typename itksys::hash_map<LabelPixelType, LabelGeometry>::const_iterator MapConstIterator;
   
   // Macros for enabling the calculation of additional features.
   itkGetMacro(CalculatePixelIndices, bool);
